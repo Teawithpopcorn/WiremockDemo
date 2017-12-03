69c1b390d4427100a1f4ae3b96187a7adc773215
TaoJuanJuan <tjj612@163.com>
2017-12-03 23:22:51 +0800

    submit all files

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,49 @@
+package demo;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+public class Demo {
+    private static int port = 2345;
+    private static String serverAddress = "localhost";
+    private static WireMockServer wireMockServer = new WireMockServer(options().port(port));
+
+    //curl localhost:2345/api/demo1
+    public static void stubForGet() {
+        stubFor(get(urlMatching("/api/demo1"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withStatusMessage("Everything was just fine!")
+                        .withHeader("Content-Type", "text/plain")
+                        .withBody("Stub For demo1 (Java)")));
+    }
+
+    //curl -X POST localhost:2345/api/demo2
+    public static void stubForPost() {
+        stubFor(post(urlMatching("/api/demo2"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody("Stub For Demo2 (Java)")));
+    }
+
+    //curl -X POST --data apple localhost:2345/api/demo3
+    public static void stubForPostWithReq() {
+        stubFor(post(urlMatching("/api/demo3"))
+                .withRequestBody(equalTo("apple"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody("Stub For Demo3 (Java)")));
+    }
+
+    public static void main(String[] args){
+        configureFor(serverAddress, port);
+        wireMockServer.start();
+        stubForGet();
+        stubForPost();
+        stubForPostWithReq();
+
+        System.out.print("===== Mock Service started =====");
+    }
+}
---
+++ src/main/java/hello/MyDemo.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class MyDemo {
+
+    public static void main(String[] args){
+        System.out.print("===== Mock Service started =====");
+    }
+}
