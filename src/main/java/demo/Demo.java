7689de536149816cf2e821b920fcc1d3da5ddce4
TaoJuanJuan <tjj612@163.com>
2017-12-04 18:04:56 +0800

    prefill MyDemo

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,5 +1,6 @@
 package demo;
 
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
--- src/main/java/hello/MyDemo.java
+++ src/main/java/hello/MyDemo.java
@@ -1,8 +1,20 @@
 package hello;
 
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
 public class MyDemo {
+    private static WireMockServer wireMockService = new WireMockServer(2345);   //port is 8080 by default
+
+    public static void Demo1(){
+
+    }
+
 
     public static void main(String[] args){
+        wireMockService.start();
+        Demo1();
+
         System.out.print("===== Mock Service started =====");
     }
 }
